a19de0da0dec22a9b9ae50d3c8b6447daa3bcc6e
Joosep <jjuripuusaag@gmail.com>
2024-04-03 16:58:15 +0300

    add week7 code

---
+++ argumentsandparameters/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter something:");
+string userInput = Console.ReadLine();
+
+printAnyWord(userInput);
+
+static void printAnyWord(string anyString)
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are you leaving or staying? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    printHello();
+}
+if (userChoice == "out")
+{
+    printGoodBye();
+}
+else
+{
+    Console.WriteLine("Invalid input.");
+}
+
+static void printHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void printGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
